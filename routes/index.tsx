c5a1af29655067cef4453062ebd9a1319cf4f61a
迷渡 <justjavac@gmail.com>
2022-07-11 09:33:17 +0800

    fix format
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,19 +12,23 @@ export default function Home() {
       </Head>
       <div class={tw`p-4 mx-auto max-w-screen-md`}>
         <header
-          class={tw`relative section-x-inset-sm pt-10 pb-20 flex flex-col items-center`}
+          class={tw
+            `relative section-x-inset-sm pt-10 pb-20 flex flex-col items-center`}
         >
           <h1
-            class={tw`font-extrabold text-5xl leading-10 tracking-tight text-gray-900`}
+            class={tw
+              `font-extrabold text-5xl leading-10 tracking-tight text-gray-900`}
           >
             Deno PKG
           </h1>
           <h2
-            class={tw`mt-6 sm:mt-8 font-light text-2xl text-center leading-tight text-gray-900`}
+            class={tw
+              `mt-6 sm:mt-8 font-light text-2xl text-center leading-tight text-gray-900`}
           >
-            A <strong class={tw`font-semibold`}>version semantic</strong> and{" "}
-            <strong class={tw`font-semibold`}>fast</strong> package delivery
-            network for Deno.
+            A <strong class={tw`font-semibold`}>version semantic</strong> and
+            {" "}
+            <strong class={tw`font-semibold`}>fast</strong>{" "}
+            package delivery network for Deno.
           </h2>
         </header>
         <h3 class={tw`font-bold text-xl`}>Examples</h3>
@@ -85,7 +89,8 @@ export default function Home() {
           </li>
         </ul>
         <p class={tw`py-2 mt-4`}>
-          Using a <strong class={tw`font-semibold`}>tag</strong> (only support{" "}
+          Using a <strong class={tw`font-semibold`}>tag</strong> (only support
+          {" "}
           <code class={tw`py-1 px-2 font-mono bg-gray-100 text-sm break-word`}>
             latest
           </code>
